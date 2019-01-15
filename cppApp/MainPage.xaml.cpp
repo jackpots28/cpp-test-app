0889e35af53e514053e775bc1a70237aab8e639d
Jack Sims <24850877+jackpots28@users.noreply.github.com>
2019-01-15 16:28:53 -0600

    Add project files.

---
+++ cppApp/MainPage.xaml.cpp
@@ -0,0 +1,46 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <iostream>
+
+using namespace cppApp;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+int counter = 0;
+int c = 0;
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void cppApp::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	String^ s = textBox->Text;
+	int a = 0;
+	int b = 0;
+
+	a = _wtof(s->Data());
+	b = a;
+	c += a;
+	button->Content = counter.ToString();
+	textBlock->Text = c.ToString();
+	counter++;
+	
+}
+
+
